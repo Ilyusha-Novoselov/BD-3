169338912471906017a443f80d0b9cf0f738ae81
Ilyusha-Novoselov <ilyanov56@mail.ru>
2024-10-13 15:36:19 +0700

    Added functionality

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "functions.h"
 
 int main() {
@@ -8,7 +10,29 @@ int main() {
     connect_db(login, password);
     connect_schema(schema);
 
-    table_rollback();
+    int n;
+    while(1) {
+        printf("\nВведите номер задания:\n[1-5] - номера заданий\n0 - выход\n6 - сброс таблиц до начального состояния\n");
+        scanf("%d", &n);
+        printf("Результат:\n");
+        switch(n) {
+            case 0:
+                printf("Завершение программы\n");
+                return 0;
+            case 1:
+                task_1();
+                break;
+            case 2:
+                task_2();
+                break;
+            case 6:
+                table_rollback();
+                break;
+            default:
+                printf("Неверный ввод номера задания\n");
+                break;
+        }
+    }
 
     return 0;
 }
